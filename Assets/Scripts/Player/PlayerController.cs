58d0d36f9e9ac6485378617946472bcec2ca76a8
rfasquelle <raphaelle.fasquelle@musicworldmedia.com>
2023-05-27 15:43:14 +0200

    [ANIMATIONS] Add run and idle animation + rotation to base control

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,11 +5,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private enum PlayerState
+    {
+        Idle,
+        Attacking,
+        Moving,
+    }
+    
     [SerializeField] private  float _movementSpeed;
+    [SerializeField] private  float _rotationSpeed;
     [SerializeField, Range(0f,1f)] private  float _minimumMoveInput = .3f;
+    [SerializeField] private Animator _animator;
     
     private VirtualJoystick _joystick;
     private float _movementCameraMultiplier;
+
+    private PlayerState _currentState;
+    
+    //animator keys
+    private int _moveAnimationKey = Animator.StringToHash("IsMoving");
     
     public void Init()
     {
@@ -24,6 +38,8 @@ public class PlayerController : MonoBehaviour
         
         _joystick = GameManager.Instance.Joystick;
         _movementCameraMultiplier = GameManager.Instance.Camera.transform.forward.z > 0 ? 1 : -1;
+
+        _currentState = PlayerState.Idle;
     }
 
     private void Update()
@@ -35,9 +51,37 @@ public class PlayerController : MonoBehaviour
     {
         if (_joystick.JoystickInput.magnitude > _minimumMoveInput)
         {
+            if (_currentState != PlayerState.Moving)
+            {
+                StartMoving();
+            }
+
+            transform.forward = Vector3.RotateTowards(transform.forward,
+                _joystick.JoystickInput, _rotationSpeed * Time.deltaTime,
+                1);
             transform.position = Vector3.MoveTowards(transform.position
                 , transform.position + _joystick.JoystickInput * (_movementCameraMultiplier * 10),
                 Time.deltaTime * _movementSpeed);
         }
+        else
+        {
+            if (_currentState == PlayerState.Moving)
+            {
+                StopMoving();
+            }
+        }
+    }
+
+    private void StartMoving()
+    {
+        _currentState = PlayerState.Moving;
+        _animator.SetBool(_moveAnimationKey, true);
+    }
+
+    private void StopMoving()
+    {
+        //TODO: CHECK HERE IF TARGET TO SET STATE TO IDLE OR ATTACKING
+        _currentState = PlayerState.Idle;
+        _animator.SetBool(_moveAnimationKey, false);
     }
 }
