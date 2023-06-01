e416682cdde9ec83f6f401c0913aff18c2930ad5
rfasquelle <raphaelle.fasquelle@musicworldmedia.com>
2023-06-01 18:45:27 +0200

    [REFACTO] Refacto flying text

--- Assets/Scripts/UI/FlyingText3d.cs
+++ Assets/Scripts/UI/FlyingText3d.cs
@@ -10,19 +10,28 @@ public class FlyingText3d : MonoBehaviour
 {
     [SerializeField] private TextMeshPro _text;
 
-    public void DoFly(string value, Vector3 startPos, Color color, Action onComplete)
+    private Sequence _flySequence;
+    
+    public void DoFly(string value, Vector3 startPos, Color color, Action completionCallback)
     {
         transform.position = startPos;
         _text.SetText(value);
         _text.color = color;
         _text.alpha = 0;
         transform.localScale = Vector3.zero;
-        Sequence flySequence = DOTween.Sequence();
-        flySequence.Append(_text.DOFade(1, .15f));
-        flySequence.Join(transform.DOScale(1f, .25f));
-        flySequence.AppendInterval(.5f);
-        flySequence.Append(_text.DOFade(0, .3f));
-        flySequence.Join(transform.DOScale(0, .3f));
-        flySequence.OnComplete(onComplete.Invoke);
+        DoFlySequence(completionCallback);
+    }
+
+    private void DoFlySequence(Action completionCallback)
+    {
+        if(_flySequence != null) _flySequence.Kill(true);
+        
+        _flySequence = DOTween.Sequence();
+        _flySequence.Append(_text.DOFade(1, .15f));
+        _flySequence.Join(transform.DOScale(1f, .25f));
+        _flySequence.AppendInterval(.5f);
+        _flySequence.Append(_text.DOFade(0, .3f));
+        _flySequence.Join(transform.DOScale(0, .3f));
+        _flySequence.OnComplete(completionCallback.Invoke);
     }
 }
